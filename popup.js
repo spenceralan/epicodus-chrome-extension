ed4c5a0705940d2f5170efc5bfba50828633c9ab
Kai de Rochemont and Spencer Alan <kai+spencer@>
2017-06-15 17:03:07 -0700

    connect form for secondary student

--- popup.js
+++ popup.js
@@ -10,19 +10,25 @@
 
 $(document).ready(function() {
   $('#attendance-in').click(function(){
-    var email = $('#email').val();
-    var password = $('#password').val();
+    var email1 = $('#email-1').val();
+    var password1 = $('#password-1').val();
+    var email2 = $('#email-2').val();
+    var password2 = $('#password-2').val();
     var station = $('#station').val();
     chrome.tabs.create({url: "https://epicenter.epicodus.com/sign_in"}, function(){
       chrome.tabs.executeScript(
         null,
         {code:`
-          var email = document.getElementById('email1');
-          var password = document.getElementById('password1');
+          var email1 = document.getElementById('email1');
+          var password1 = document.getElementById('password1');
+          var email2 = document.getElementById('email2');
+          var password2 = document.getElementById('password2');
           var station = document.getElementById('station');
           var submit  = document.querySelectorAll('input[type="submit"]')[0];
-          email.value = '${email}';
-          password.value = '${password}';
+          email1.value = '${email1}';
+          password1.value = '${password1}';
+          email2.value = '${email2}';
+          password2.value = '${password2}';
           station.value = '${station}';
           submit.click();
         `}
